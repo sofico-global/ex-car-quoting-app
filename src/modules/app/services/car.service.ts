451c5504fbf82ab4b78905f688f330098ac816a5
ruve <ruben.vermeulen@sofico.be>
2020-02-13 20:36:03 +0100

    changes car service api src

--- src/modules/app/services/car.service.ts
+++ src/modules/app/services/car.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import {
-  Observable,
-  of
-} from 'rxjs';
-import { Car } from '../types/car.type';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Car} from '../types/car.type';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class CarService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+  }
 
   find(): Observable<Car[]> {
-    return this.httpClient.get<Car[]>(environment.baseUrl + 'cars');
+    return this.httpClient.get<Car[]>('https://api.jsonbin.io/b/5e45a45fd18e4016617821ce');
   }
 
   findOne(carId: string): Observable<Car> {
-    return this.httpClient.get<Car>(environment.baseUrl + 'cars/' + carId);
+    return this.httpClient.get<Car[]>('https://api.jsonbin.io/b/5e45a45fd18e4016617821ce').pipe(
+      map(cars => cars.find(car => car.carId === carId))
+    );
   }
 }
