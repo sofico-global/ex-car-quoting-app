c02b9e552db31d39ba81aee4a231d9781314fe24
Ruben Vermeulen <RubenVermeulen@users.noreply.github.com>
2022-02-10 08:49:01 +0100

    Update to Angular 13

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {paramsInheritanceStrategy: 'always'})],
+  imports: [RouterModule.forRoot(routes, { paramsInheritanceStrategy: 'always', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
